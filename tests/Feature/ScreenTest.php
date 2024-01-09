49c342398c8019b37ae54b5ed6e184ba7865b7ac
Aliakseyenka Ihar <aliha.devs@gmail.com>
2024-01-09 17:59:25 +0300

    tests: handle exception

--- tests/Feature/ScreenTest.php
+++ tests/Feature/ScreenTest.php
@@ -1,14 +1,17 @@
 <?php
 
+use Orchid\Support\Testing\DynamicTestScreen;
+
 describe('screen', function () {
     it('shows screen when discover option is set to true', function () {
         $this->withoutExceptionHandling();
 
         /** @var DynamicTestScreen $screen */
-        $screen = $this->screen('platform.logs')
-                    ->actingAs($this->admin());
+        $screen = $this->screen('platform.logs');
 
-        $response = $screen->display();
+        $response = $screen
+                        ->actingAs($this->admin())
+                        ->display();
 
         // Heading
         $response->assertSee(sprintf('<h1 class="m-0 fw-light h3 text-black">%s</h1>', __('Logs')), false);
